8c6dc4cf6a99d519ce4da34b28f4bdacf93a61a0
Guozheng Fan <gfan@uni-bremen.de>
2020-06-26 11:31:58 +0200

    modified:   dftbtorch/__init__.py 	modified:   ml/__init__.py

--- dftbtorch/__init__.py
+++ dftbtorch/__init__.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-'''Global imports for dftbtorch'''
-
-from __future__ import absolute_import
-import os
-import sys
-sys.path.append(os.path.join('../../dftb'))
-sys.path.append('../dftbtorch')
-sys.path.append('../ml')
-from dftbtorch.dftb_torch import Initialization
+# from __future__ import absolute_import
+# import os
+# import sys
+# sys.path.append(os.path.join('../../dftb'))
+# sys.path.append('../dftbtorch')
+# sys.path.append('../ml')
+# from dftbtorch.dftb_torch import Initialization
 # import dftb.dftbtorch.periodic as periodic
 # import dftb.dftbtorch.electront as electront
 # import dftb.dftbtorch.readt as readt
