3b40429daeeac03e46b3459d4b70b1c27e7cf490
Guozheng Fan <gfan@uni-bremen.de>
2020-11-06 15:51:41 +0100

    get reference   	new file:   utils/get_hdf_skf.py 	new file:   utils/get_reference_data.py

---
+++ utils/get_hdf_skf.py
@@ -0,0 +1,327 @@
+"""Write skf to hdf5 binary file.
+
+The skf include normal skf files or skf with a list of compression radii.
+"""
+import os
+import h5py
+import numpy as np
+import dftbtorch.initparams as initpara
+import IO.readt as readt
+import dftbtorch.parameters as constpara
+VAL_ORB = {"H": 1, "C": 2, "N": 2, "O": 2, "Ti": 3}
+
+
+class WriteSKNormal:
+    """Transfer SKF files from skf files to hdf binary type.
+
+    Args:
+        para: general parameter dictionary.
+
+    """
+
+    def __init__(self, para):
+        """Read skf, smooth the tail, write to hdf."""
+        self.para = para
+        self.ml = {}
+        self.para['directorySK'] = '../slko/test/'
+        self.dataset = {}
+        self.dataset['Dataset'] = '../data/dataset/testfile.hdf5'
+
+        # load constant parameters
+        constpara.constant_parameter(self.para)
+
+        # get default general parameters
+        self.para = initpara.dftb_parameter(self.para)
+        self.skf = initpara.skf_parameter()
+        self.dataset = initpara.init_dataset(self.dataset)
+        self.para, self.dataset, self.skf, self.ml = \
+            initpara.init_ml(self.para, self.dataset, self.skf, self.ml)
+        self.ibatch = 0
+
+        # read the dataset and then read the corresponding skf files
+        self.read_dataset()
+
+        # interpolate integrals
+        self.interpskf()
+
+    def read_dataset(self):
+        """Read the corresponding hdf data of geometries.
+
+        Notes:
+            define para['dataType'] == 'hdf'
+        """
+        # join the path and hdf data
+        hdf_ = self.dataset['Dataset']
+
+        # read the hdf data, get the global atom specie
+        with h5py.File(hdf_, 'r') as f:
+            self.specie_global = f['globalgroup'].attrs['specie_all']
+            self.dataset['specieGlobal'] = [self.specie_global]
+
+    def interpskf(self):
+        """Read .skf data from skgen with various compR."""
+        os.system('rm skfsingle.hdf5')
+        # optimize wavefunction compression radius
+
+        # read skf according to atom specie
+        for namei in self.specie_global:
+            for namej in self.specie_global:
+
+                # generate folder name
+                dire = self.para['directorySK'] + '/' + namei + '-' + namej + '.skf'
+
+                # get integral by interpolation
+                # read normal skf file by atom specie
+                readt.ReadSlaKo(self.para,
+                                self.dataset, self.skf).read_sk_specie()
+
+                # write intepolation data as hdf
+                self.write_hdf5(namei, namej)
+
+    def write_hdf5(self, namei, namej):
+        """Write each atom pair skf to hdf."""
+        nameij = namei + namej
+
+        # if onsite is fixed (False) or tunable (True)
+        if self.skf['Lonsite']:
+            name_onsite = 'onsite'
+            onsite_ = self.skf['onsite' + nameij]
+
+            # mass and r_cut
+            name_massr = 'massrcut'
+            mass_rcut_ = self.skf['massrcut' + nameij]
+
+            # spe
+            namespe = 'spe'
+            spe_ = self.skf['spe' + nameij]
+
+            # Hubbert
+            namehubbert = 'uhubb'
+            hubbert = self.skf['uhubb' + nameij]
+
+        # gridmesh distance
+        namedist = 'grid_dist'
+        distance = self.skf['grid_dist' + nameij]
+
+        # number of gridmesh
+        namengridpoint = 'ngridpoint'
+        ngridpoint = self.skf['ngridpoint' + nameij]
+
+        # integrals
+        namehs = 'hs_all'
+        hs = self.skf['hs_all' + nameij]
+
+        if self.para['Lrepulsive']:
+
+            namenint = 'nint_rep'
+            nint_ = self.skf['nint_rep' + nameij]
+
+            namenInt_cutoff = 'cutoff_rep'
+            nInt_cutoff = self.skf['cutoff_rep' + nameij]
+
+            a1 = self.skf['a1_rep' + nameij]
+            a2 = self.skf['a2_rep' + nameij]
+            a3 = self.skf['a3_rep' + nameij]
+            rep = self.skf['rep' + nameij]
+            repend = self.skf['repend' + nameij]
+
+        # write the above to hdf
+        with h5py.File('skfsingle.hdf5', 'a') as f:
+            print(nameij)
+            g = f.create_group(nameij)
+
+            # if onsite is fixed (False) or tunable (True)
+            if self.skf['Lonsite']:
+                g.create_dataset(name_onsite, data=onsite_)
+                g.create_dataset(name_massr, data=mass_rcut_)
+                g.create_dataset(namespe, data=spe_)
+                g.create_dataset(namehubbert, data=hubbert)
+            g.create_dataset(namedist, data=distance)
+            g.create_dataset(namengridpoint, data=ngridpoint)
+            g.create_dataset(namehs, data=hs)
+
+            # create global group
+            try:
+                glob = f.create_group('globalgroup')
+                glob.attrs['grid_dist'] = distance[0, 0]
+            except:
+                pass
+
+
+class WriteSKComprR:
+    """Transfer SKF files from skf files to hdf binary type.
+
+    Args:
+        para (dict): general parameter
+        dataset (dict): dataset parameter
+    """
+
+    def __init__(self, para):
+        """Read skf, smooth the tail, write to hdf."""
+        self.para = para
+        self.ml = {}
+        self.para['directorySK'] = '../slko/uniform/'
+
+        # load initial parameters
+        self.para = initpara.dftb_parameter(self.para)
+        self.skf = initpara.skf_parameter()
+        self.dataset = initpara.init_dataset()
+        self.para, self.dataset, self.skf, self.ml = \
+            initpara.init_ml(self.para, self.dataset, self.skf, self.ml)
+
+        # path
+        path = os.getcwd()
+        self.hdf_ = os.path.join(path, 'testfile.hdf5')
+        # load constant parameters
+        constpara.constant_parameter(self.para)
+
+        # read the dataset and then read the corresponding skf files
+        self.readdataset()
+
+        # interpolate integrals
+        self.interpskf()
+
+    def readdataset(self):
+        """Read the corresponding hdf data of geometries.
+
+        Notes:
+            define para['dataType'] == 'hdf'
+        """
+        # read the hdf data, get the global atom specie
+        with h5py.File(self.hdf_, 'r') as f:
+            self.specie_global = f['globalgroup'].attrs['specieGlobal']
+
+    def interpskf(self):
+        """Read .skf data from skgen with various compR."""
+        os.system('rm skf.hdf5')
+        # optimize wavefunction compression radius
+        if self.ml['typeSKinterpR'] == 'wavefunction':
+            nametail = '_wav'
+
+        # optimize density compression radius
+        elif self.ml['typeSKinterpR'] == 'density':
+            nametail = '_den'
+
+        # optimize all the compression radius and keep all the same
+        elif self.ml['typeSKinterpR'] == 'all':
+            nametail = '_all'
+
+        # read skf according to atom specie
+        for namei in self.specie_global:
+            for namej in self.specie_global:
+
+                # get atom number and read corresponding directory
+                if self.para['atomno_' + namei] < self.para['atomno_' + namej]:
+
+                    # generate folder name
+                    dire = self.para['directorySK'] + '/' + namei + \
+                        '_' + namej + nametail
+
+                    # get integral by interpolation
+                    readt.SkInterpolator(
+                        self.para, self.dataset,
+                        self.skf, gridmesh=0.2).readskffile(
+                            namei, namej, dire)
+
+                    # write intepolation data as hdf
+                    self.write_hdf5(namei, namej)
+                else:
+
+                    # generate folder name
+                    dire = self.para['directorySK'] + '/' + namej + \
+                        '_' + namei + nametail
+
+                    # get integral by interpolation
+                    readt.SkInterpolator(
+                        self.para, self.dataset,
+                        self.skf, gridmesh=0.2).readskffile(
+                            namei, namej, dire)
+
+                    # write intepolation data as hdf
+                    self.write_hdf5(namei, namej)
+
+    def write_hdf5(self, namei, namej):
+        """Write each atom pair skf to hdf."""
+        nameij = namei + namej
+
+        # if onsite is fixed (False) or tunable (True)
+        if self.skf['Lonsite']:
+            name_onsite = 'onsite_rall'
+            onsite_ = self.skf['onsite_rall' + nameij]
+
+            # mass and r_cut
+            name_massr = 'massrcut_rall'
+            mass_rcut_ = self.skf['massrcut_rall' + nameij]
+
+            # spe
+            namespe = 'spe_rall'
+            spe_ = self.skf['spe_rall' + nameij]
+
+            # Hubbert
+            namehubbert = 'uhubb_rall'
+            hubbert = self.skf['uhubb_rall' + nameij]
+
+        # gridmesh distance
+        namedist = 'grid_dist_rall'
+        distance = self.skf['grid_dist_rall' + nameij]
+
+        # number of gridmesh
+        namengridpoint = 'ngridpoint_rall'
+        ngridpoint = self.skf['ngridpoint_rall' + nameij]
+
+        # integrals
+        namehs = 'hs_all_rall'
+        hs = self.skf['hs_all_rall' + nameij]
+
+        # number of compression radius
+        namecompr = 'ncompr'
+        compr = int(np.sqrt(self.skf['nfile_rall' + nameij]))
+
+        if self.para['Lrepulsive']:
+
+            namenint = 'nint_rep'
+            nint_ = self.skf['nint_rep' + nameij]
+
+            namenInt_cutoff = 'cutoff_rep'
+            nInt_cutoff = self.skf['cutoff_rep' + nameij]
+
+            a1 = self.skf['a1_rep' + nameij]
+            a2 = self.skf['a2_rep' + nameij]
+            a3 = self.skf['a3_rep' + nameij]
+            rep = self.skf['rep' + nameij]
+            repend = self.skf['repend' + nameij]
+
+        # write the above to hdf
+        with h5py.File('skf.hdf5', 'a') as f:
+            print(nameij)
+            g = f.create_group(nameij)
+
+            # if onsite is fixed (False) or tunable (True)
+            if self.skf['Lonsite']:
+                g.create_dataset(name_onsite, data=onsite_)
+                g.create_dataset(name_massr, data=mass_rcut_)
+                g.create_dataset(namespe, data=spe_)
+                g.create_dataset(namehubbert, data=hubbert)
+            g.create_dataset(namedist, data=distance)
+            g.create_dataset(namengridpoint, data=ngridpoint)
+            g.create_dataset(namehs, data=hs)
+            g.attrs[namecompr] = compr
+
+            # create global group
+            try:
+                glob = f.create_group('globalgroup')
+                glob.attrs['grid_dist'] = distance[0, 0]
+            except:
+                pass
+
+    def read_skf(self):
+        pass
+
+
+if __name__ == '__main__':
+    para = {}
+    para['task'] = 'get_hdf_compr'
+    if para['task'] == 'get_hdf_compr':
+        WriteSKComprR(para)
+    elif para['task'] == 'get_hdf_normal':
+        WriteSKNormal(para)
---
+++ utils/get_reference_data.py
@@ -0,0 +1,188 @@
+"""Build hdf type data for machine learning reference.
+
+The data will include:
+    geometry information,
+    physical properties
+
+define para['dataType'] as original dataset, e.g 'ani'
+
+"""
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import numpy as np
+import h5py
+import dftbtorch.initparams as initpara
+import IO.pyanitools as pya
+from utils.aset import DFTB, AseAims
+from IO.dataloader import LoadData
+import dftbtorch.parameters as constpara
+ATOMNUM = {'H': 1, 'C': 6, 'N': 7, 'O': 8}
+
+
+class RefDFTB:
+    """Physical properties are from DFTB-ASE calculations.
+
+    Note:
+    -----
+    set para['reference'] = 'dftbase'
+    """
+
+    def __init__(self, para):
+        """Load original dataset and then write into hdf type."""
+        # define ml dictionary
+        self.para = para
+
+        # load constant parameters
+        constpara.constant_parameter(self.para)
+        self.ml = {}
+        self.dataset = {}
+        self.ml['reference'] = 'dftbase'
+        self.dataset['dataType'] = 'ani'
+        self.dataset['LdatasetMixture'] = False
+
+        # get parameters for generating reference data
+        self.para = initpara.dftb_parameter(self.para)
+        self.skf = initpara.skf_parameter()
+        self.dataset = initpara.init_dataset(self.dataset)
+        self.para, self.dataset, self.skf, self.ml = \
+            initpara.init_ml(self.para, self.dataset, self.skf, self.ml)
+
+        # load hdf data
+        LoadData(self.para, self.dataset, self.ml).load_data_hdf()
+
+    def load_data_hdf(self):
+        """Load data from original dataset, ani ..."""
+        os.system('rm testfile.hdf5')
+
+        # the global atom species in dataset (training data)
+        self.para['specie_all'] = []
+
+        # number of molecules in dataset
+        nmol = 0
+        for hdf5file in self.para['hdffile']:
+            adl = pya.anidataloader(hdf5file)
+            for data in adl:
+
+                # this loop will write information of the same molecule with
+                # different geometries
+                atom_num = []
+
+                # get atom number of each atom
+                [atom_num.append(ATOMNUM[spe]) for spe in data['species']]
+
+                # number of molecule in current molecule species
+                imol = len(data['coordinates'])
+
+                # number of atom in molecule
+                natom = len(data['coordinates'][0])
+
+                # write all coordinates to list "corrall" and first column is
+                # atom number
+                self.para['coorall'] = []
+                self.para['coorall'].extend(
+                    [np.insert(coor, 0, atom_num, axis=1)
+                     for coor in np.asarray(data['coordinates'], dtype=float)])
+
+                # write the current atom species in molecule to list "symbols"
+                # use metadata instead
+                # self.para['symbols'].extend(
+                #     data['species'] for ii in range(imol))
+                ispecie = ''.join(data['species'])
+
+                # write global atom species
+                for ispe in data['species']:
+                    if ispe not in self.para['specie_all']:
+                        self.para['specie_all'].append(ispe)
+
+                # write number of atoms
+                # self.para['natomall'].extend(natom for ii in range(imol))
+
+                # write metadata
+                with h5py.File('testfile.hdf5', 'a') as self.f:
+                    self.g = self.f.create_group(ispecie)
+                    self.g.attrs['specie'] = ispecie
+                    self.g.attrs['natom'] = natom
+
+                    # run reference claculations
+                    try:
+                        if self.para['reference'] == 'dftbase':
+                            self.run_dftbplus(imol, self.para['coorall'])
+                    except ValueError:
+                        print("Please set reference == dftbase!")
+
+                nmol += imol
+
+        # save rest to global attrs
+        with h5py.File('testfile.hdf5', 'a') as f:
+            g = f.create_group('globalgroup')
+            g.attrs['specie_all'] = self.para['specie_all']
+            g.attrs['totalmolecule'] = nmol
+
+    def run_dftbplus(self, end, coorall):
+        """Run DFTB-ase and save as hdf data."""
+        # DFTB+ as reference
+        DFTB(self.para, setenv=True).run_dftb(end, coorall, hdf=self.f,
+                                              group=self.g)
+
+    def run_dftbase():
+        pass
+
+    def get_property():
+        pass
+
+    def save_data():
+        pass
+
+
+class RefAims:
+    """Run FHI-aims calculations and save results as reference data for ML.
+
+    Note:
+    -----
+    set para['reference'] = 'aimsase'
+    """
+
+    def __init__(self, para):
+        """Load geometry data, run FHI-aims and analyze the results."""
+        # load constant parameters
+        constpara.constant_parameter()
+
+        # define ml dictionary
+        self.para = para
+        self.ml = {}
+        self.dataset = {}
+        self.ml['reference'] = 'aimsase'
+        self.dataset['datasetType'] = 'runani'
+
+        # read and run different molecule species dataset size
+        self.dataset['sizeDataset'] = ['2']
+        self.dataset['LdatasetMixture'] = False
+        self.dataset['dataset'] = ['../data/dataset/an1/ani_gdb_s01.h5']
+
+        # get parameters for generating reference data
+        self.para = initpara.dftb_parameter(self.para)
+        self.skf = initpara.skf_parameter(self.para)
+        self.dataset = initpara.init_dataset(self.dataset)
+        self.para, self.dataset, self.skf, self.ml = \
+            initpara.init_ml(self.para, self.dataset, self.skf, self.ml)
+
+        # LoadData will load ani dataset (call function load_ani)
+        # then run load_data_hdf
+        LoadData(self.para, self.dataset, self.ml)
+
+    def load_data_hdf(self):
+        """Load original data for FHI-aims calculations."""
+        pass
+
+    def run_aims(self):
+        """Run FHI-aims calculations."""
+        pass
+
+if __name__ == '__main__':
+    para = {}
+    para['task'] = 'get_aims_hdf'
+    if para['task'] == 'get_dftb_hdf':
+        RefDFTB(para)
+    elif para['task'] == 'get_aims_hdf':
+        RefAims(para)
