73fb459a338706a6ed1b89897caff2fc1be48e37
Guozheng Fan <gfan@uni-bremen.de>
2020-11-17 20:21:35 +0100

    modified:   init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,7 @@ export PATH=$PWD:$PATH
 export PYTHONPATH=$PWD:$PYTHONPATH
 #export PATH=$PWD/dftbtorch:$PYTHONPATH
 export PYTHONPATH=$PWD/DFTBMaLT:$PYTHONPATH
+
+# CCS
+export PYTHONPATH=/home/gz_fan/Documents/ML/dftb/utils/CCS:$PYTHONPATH
+export PATH=/home/gz_fan/Documents/ML/dftb/utils/CCS/bin:$PATH
