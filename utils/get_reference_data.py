8377b1bdcaef2e08452dce5963c4ca6f19c288ae
Guozheng Fan <gfan@uni-bremen.de>
2020-11-21 22:50:41 +0100

    modified:   dftb_in.mlCompressionR 	modified:   ../utils/get_reference_data.py

--- utils/get_reference_data.py
+++ utils/get_reference_data.py
@@ -99,8 +99,8 @@ class RefAims:
         self.dataset['LdatasetMixture'] = False
 
         # define dataset as input
-        self.dataset['dataset'] = '../data/dataset/an1/ani_gdb_s03.h5'
-        self.dataset['sizeDataset'] = [1] * 25
+        self.dataset['dataset'] = '../data/dataset/an1/ani_gdb_s01.h5'
+        self.dataset['sizeDataset'] = [2000] * 25
 
         # get parameters for generating reference data
         self.para = initpara.dftb_parameter(self.para)
